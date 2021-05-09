a93ecebad0d719a99919886850475b1f6aca6217
AndreiBadescu <34285711+AndreiBadescu@users.noreply.github.com>
2021-05-09 13:55:02 +0300

    Update MemoryHandler.hpp

--- MemoryHandler.hpp
+++ MemoryHandler.hpp
@@ -1,28 +1,28 @@
 /*
-    Functions:
-    - MemoryHandler::MemoryUsage() : to show how much memory is currently dynamic allocated
-    - MemoryHandler::Allocations() : to show how many allocations and deallocations you've done
-    - MemoryHandler::UnfreedAllocations() : to show how many allocations are still alive (unfreed)
-    - MemoryHandler::ShowAll() : calls all the above functions (in this order)
+Functions:
+- MemoryTracker::MemoryUsage() : to show how much memory is currently dynamic allocated;
+- MemoryTracker::Allocations() : to show how many allocations and deallocations you've done;
+- MemoryTracker::UnfreedAllocations() : to show how many allocations are still alive (unfreed);
+- MemoryTracker::ShowAll() : calls all the above functions (in this order).
 
-    Option:
-    If you want to print a message with how much memory you allocate every time
-    go to line 5 and change "#define MESSAGES_ON false" to "#define MESSAGES_ON true"
-    Format of a printed message in console is: "<FILENAME> <LINE>: <Message>"
+Option:
+If you want to print a message with how much memory you allocate every time
+go to line 5 and change "#define MESSAGES_ON false" to "#define MESSAGES_ON true".
+Format of a printed message in console is: "<FILENAME> <LINE>: <Message>".
 
-    How is done:
-    I overloaded the new, new[], delete and delete[] operators.
-    You can do maximum 10,000 unfreed allocations before you exceed the limit
-    (if you want to increase / decrease this limit go to line 4 and change the number)
+How is done:
+I overloaded the new, new[], delete and delete[] operators.
+You can do maximum 10,000 unfreed allocations before you exceed the limit
+(if you want to increase / decrease this limit go to line 4 and change the number).
 
-    Others:
-    If you try to delete an array like a variable (basically, just deleting the 1st element)
-    you'll get an error message saying "YOU DELETED AN ARRAY LIKE A VARIABLE, USE []"
-    Also if you try to allocate 0 bytes you'll get a warning in console
+Others:
+If you try to delete an array like a variable (basically, just deleting the 1st element)
+you'll get an error message saying "YOU DELETED AN ARRAY LIKE A VARIABLE, USE []".
+Also if you try to allocate 0 bytes you'll get a warning in console.
 */
 
-#ifndef MEMORYHANDLER_H_INCLUDED
-#define MEMORYHANDLER_H_INCLUDED
+#ifndef MEMORYTRACKER_H_INCLUDED_
+#define MEMORYTRACKER_H_INCLUDED_
 
 // change to true to show messages in console when you allocate/deallocate memory
 #define MESSAGES_ON true
@@ -77,7 +77,7 @@
 * 
 */
 
-/* HERE BEGINS THE CODE (DON'T CHANGE IT IF YOU DON'T KNOW WHAT YOU ARE DOING)*/
+/* HERE BEGINS THE CODE (DON'T CHANGE IT IF YOU DON'T KNOW WHAT YOU DOING)*/
 
 /* This dissables 2 warnings related to this file in MSVC */
 /* but you also need to uncomment line 214 "//#pragma warning(pop)" */
@@ -88,16 +88,16 @@
 #include <cassert>
 #include <cstring>
 
-class MemoryHandler {
+class MemoryTracker {
 public:
-    static MemoryHandler& Get() {
-        static MemoryHandler handler;
-        return handler;
+    static MemoryTracker& Get() {
+        static MemoryTracker tracker;
+        return tracker;
     }
 
     static void Allocate(void* const adress, const std::size_t size, const bool isArray) {
         if (Get().printMessages) printf("Allocating %zu bytes\n", size);
-        if (size == 0) printf("!!!WARNING: YOU ALLOCATED 0 BYTES\n");
+        if (size == 0) printf("!!!WARNING!!!: YOU ALLOCATED 0 BYTES\n");
         Get().blocks[Get().nr_of_allocations].adress = adress;
         Get().blocks[Get().nr_of_allocations].size = size;
         Get().blocks[Get().nr_of_allocations].isArray = isArray;
@@ -160,7 +160,7 @@ private:
     int32_t nr_of_deallocations;
     memory_block* blocks;
 
-    MemoryHandler() {
+    MemoryTracker() {
         memory = 0;
         nr_of_allocations = 0;
         nr_of_deallocations = 0;
@@ -172,26 +172,26 @@ private:
 void* operator new(const std::size_t size) {
     void* ptr = malloc(size);
     assert(ptr);
-    MemoryHandler::Allocate(ptr, size, false);
+    MemoryTracker::Allocate(ptr, size, false);
     return ptr;
 }
 
 void* operator new[](const std::size_t size) {
     void* ptr = malloc(size);
     assert(ptr);
-    MemoryHandler::Allocate(ptr, size, true);
+    MemoryTracker::Allocate(ptr, size, true);
     return ptr;
 }
 
 void operator delete(void* const adress) {
     assert(adress);
-    MemoryHandler::Deallocate(adress, false);
+    MemoryTracker::Deallocate(adress, false);
     free(adress);
 }
 
 void operator delete[](void* const adress) {
     assert(adress);
-    MemoryHandler::Deallocate(adress, true);
+    MemoryTracker::Deallocate(adress, true);
     free(adress);
 }
 
@@ -212,4 +212,4 @@ void* operator new[](const std::size_t size, const int line, const char* const f
 #endif
 
 //#pragma warning(pop)
-#endif // MEMORYHANDLER_H_INCLUDED
+#endif // MEMORYTRACKER_H_INCLUDED_
